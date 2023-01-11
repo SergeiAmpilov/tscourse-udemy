f448e5f6de8322895372fd40fc4c87f8c534c938
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-11 16:34:00 +0300

    lesson 16

---
+++ 016/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const skill16 = [5, 'Bitrix'];
+const a16 = 5;
+console.log(skill16);
+// skill16[0] = 44;
+// skill16[1] = 'TypeScript';
+console.log(skill16);
+const skills16 = ['5', 'Bitrix'];
+skills16[0] = '';
---
+++ 016/app.ts
@@ -0,0 +1,13 @@
+const skill16: readonly [number, string] = [5, 'Bitrix'];
+const a16 = 5;
+
+console.log(skill16);
+
+// skill16[0] = 44;
+// skill16[1] = 'TypeScript';
+
+console.log(skill16);
+
+
+const skills16: ReadonlyArray<string> = ['5', 'Bitrix'];
+// skills16[0] = '';
