2499b62215546b4ba45f106bd57058d85f1c7936
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-16 15:28:06 +0300

    lesson 29

---
+++ 029/app.js
@@ -0,0 +1,5 @@
+"use strict";
+/* never type */
+function generateError(message) {
+    throw new Error(message);
+}
---
+++ 029/app.ts
@@ -0,0 +1,8 @@
+
+
+/* never type */
+
+
+function generateError (message: string): never {
+  throw new Error(message);
+}
