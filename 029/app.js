e44ba838d901592d9ee144ceaafd7337ba97f0f4
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-16 15:48:42 +0300

    feat: check never

--- 029/app.js
+++ 029/app.js
@@ -3,3 +3,19 @@
 function generateError(message) {
     throw new Error(message);
 }
+// const a29: never = 1;
+const b29 = generateError('try');
+function processPayment(action) {
+    switch (action) {
+        case 'refund':
+            /** */
+            break;
+        case 'checkout':
+            /** */
+            break;
+        default:
+            const _ = action; /* never as checking method*/
+            throw new Error('no such action available');
+            break;
+    }
+}
--- 029/app.ts
+++ 029/app.ts
@@ -6,3 +6,24 @@
 function generateError (message: string): never {
   throw new Error(message);
 }
+
+
+// const a29: never = 1;
+const b29 = generateError('try');
+
+
+type paymentAction = 'refund' | 'checkout';
+function processPayment(action: paymentAction) {
+  switch (action) {
+    case 'refund':
+      /** */
+      break;
+    case 'checkout':
+      /** */
+      break;
+    default:
+      const _: never = action; /* never as checking method*/
+      throw new Error('no such action available');
+      break;
+  }
+}
