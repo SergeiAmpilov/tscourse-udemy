a3f5a6094c2429b8cba9f5c3b8dc82694297035a
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-18 17:12:57 +0300

    lesson 37 p1

---
+++ 037/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var PaymentStatus37;
+(function (PaymentStatus37) {
+    PaymentStatus37[PaymentStatus37["Hold"] = 0] = "Hold";
+    PaymentStatus37[PaymentStatus37["Process"] = 1] = "Process";
+    PaymentStatus37[PaymentStatus37["Reverse"] = 2] = "Reverse";
+})(PaymentStatus37 || (PaymentStatus37 = {}));
+class Payment37 {
+}
---
+++ 037/app.ts
@@ -0,0 +1,12 @@
+enum PaymentStatus37 {
+  Hold,
+  Process,
+  Reverse,
+}
+
+
+class Payment37 {
+  id: number;
+  status: PaymentStatus37;
+
+}
