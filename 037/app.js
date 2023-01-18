ef429132dce6c71638dde56fbd6efcbf4ab4ff57
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-18 22:06:43 +0300

    lesson 37 full

--- 037/app.js
+++ 037/app.js
@@ -1,9 +1,29 @@
 "use strict";
 var PaymentStatus37;
 (function (PaymentStatus37) {
-    PaymentStatus37[PaymentStatus37["Hold"] = 0] = "Hold";
-    PaymentStatus37[PaymentStatus37["Process"] = 1] = "Process";
-    PaymentStatus37[PaymentStatus37["Reverse"] = 2] = "Reverse";
+    PaymentStatus37["Hold"] = "hold";
+    PaymentStatus37["Process"] = "process";
+    PaymentStatus37["Reverse"] = "reverse";
 })(PaymentStatus37 || (PaymentStatus37 = {}));
 class Payment37 {
+    constructor(id) {
+        this.id = id;
+        this.createdAt = new Date();
+        this.status = PaymentStatus37.Hold;
+    }
+    getPaymentLifeTime() {
+        return new Date().getTime() - this.createdAt.getTime();
+    }
+    unholdPayment() {
+        if (this.status === PaymentStatus37.Process) {
+            throw new Error('payment allready processed');
+        }
+        this.status = PaymentStatus37.Reverse;
+        this.UpdatedAt = new Date();
+    }
 }
+const p37 = new Payment37(1);
+console.log(p37);
+console.log(p37.getPaymentLifeTime());
+p37.unholdPayment();
+console.log(p37);
--- 037/app.ts
+++ 037/app.ts
@@ -1,12 +1,37 @@
 enum PaymentStatus37 {
-  Hold,
-  Process,
-  Reverse,
+  Hold = 'hold',
+  Process = 'process',
+  Reverse = 'reverse',
 }
 
 
 class Payment37 {
   id: number;
-  status: PaymentStatus37;
+  status: PaymentStatus37 = PaymentStatus37.Hold;;
+  createdAt: Date  = new Date();;
+  UpdatedAt: Date;
 
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  getPaymentLifeTime(): number {
+    return new Date().getTime() - this.createdAt.getTime();
+  }
+
+  unholdPayment(): void {
+    if (this.status === PaymentStatus37.Process) {
+      throw new Error('payment allready processed')
+    }
+
+    this.status = PaymentStatus37.Reverse;
+    this.UpdatedAt = new Date();
+  }
 }
+
+const p37 = new Payment37(1);
+console.log(p37);
+console.log(p37.getPaymentLifeTime());
+p37.unholdPayment();
+console.log(p37);
+
