034c53f732409d57b1da127269240401fdb3379f
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-19 17:15:09 +0300

    lesson 41

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 "use strict";
-// const a1 = 1;
+class Payment41 {
+    constructor(id) {
+        this.status = 'new';
+        this.id = id;
+    }
+    pay() {
+        this.status = 'paid';
+    }
+}
+class PersistedPayment extends Payment41 {
+    constructor() {
+        const id = Math.random();
+        super(id);
+        this.paidAt = new Date();
+    }
+    save() {
+    }
+    pay(date) {
+        super.pay();
+        this.paidAt = date !== null && date !== void 0 ? date : new Date();
+    }
+}
+// new PersistedPayment(23).
--- app.ts
+++ app.ts
@@ -1,1 +1,36 @@
-// const a1 = 1;
+type PaymentStatus41 = 'new' | 'paid';
+
+class Payment41 {
+  id: number;
+  status: PaymentStatus41 = 'new';
+
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  pay(): void {
+    this.status = 'paid';
+  }
+}
+
+class PersistedPayment extends Payment41 {
+  databaseId: number;
+  paidAt: Date = new Date();
+
+  constructor() {
+    const id = Math.random();
+    super(id);
+  }
+
+  save(): void {
+
+  }
+
+  override pay(date?: Date) {
+    super.pay();
+    this.paidAt = date ?? new Date();
+  }
+
+}
+
+// new PersistedPayment(23).
