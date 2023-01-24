54a5fa8522a2017cb266a9ef5743331339b5d9d4
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-24 10:30:07 +0300

    lesson 46 static

--- 046/app.js
+++ 046/app.js
@@ -1,1 +1,14 @@
 "use strict";
+class UserService46 {
+    static getUser(id) {
+        UserService46.db;
+        UserService46.user;
+        UserService46.getUser(1);
+        return 1;
+    }
+}
+(() => {
+    UserService46.db = 'fnvj';
+})();
+UserService46.db;
+UserService46.getUser(3);
---
+++ 046/app.ts
@@ -0,0 +1,22 @@
+class UserService46 {
+  static db: any;
+  private static user: any;
+
+  static {
+    UserService46.db = 'fnvj';
+  }
+
+  static getUser(id:number): number {
+    UserService46.db;
+    UserService46.user;
+    UserService46.getUser(1);
+    return 1;
+  }
+
+
+  
+}
+
+
+UserService46.db;
+UserService46.getUser(3);
