d230be60b7644170140a43b168564fd882d41c28
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-02 13:06:24 +0300

    lesson 57 shortcut

--- 057/app.js
+++ 057/app.js
@@ -1,1 +1,10 @@
 "use strict";
+class User57 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+console.log('hw 57');
+const u_57 = new User57('Ivan Fedorovich', 67);
+console.log(u_57);
---
+++ 057/app.ts
@@ -0,0 +1,11 @@
+class User57 {
+  constructor (public name: string, public age: number) {
+  }
+}
+
+
+console.log('hw 57');
+
+
+const u_57 = new User57('Ivan Fedorovich', 67);
+console.log(u_57);
