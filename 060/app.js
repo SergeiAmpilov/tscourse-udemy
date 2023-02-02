14f2b5cd5ab460a287ef8c0ee35e6c96bbed8082
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-02 14:49:09 +0300

    lesson 60 keyses

--- 060/app.js
+++ 060/app.js
@@ -7,3 +7,12 @@ const res60 = logMiddleware(10);
 console.log(typeof res60);
 const res59 = logMiddleware('sdf');
 console.log(typeof res59);
+function getSplitedHalf(data) {
+    return data.filter((el, index) => index < data.length / 2);
+}
+console.log(getSplitedHalf([1, 2, 3, 4, 5]));
+getSplitedHalf([
+    'dd',
+    'sd',
+    'dscbhsd'
+]);
--- 060/app.ts
+++ 060/app.ts
@@ -8,3 +8,19 @@ const res60 = logMiddleware(10);
 console.log(typeof res60);
 const res59 = logMiddleware<string>('sdf');
 console.log(typeof res59);
+
+
+function getSplitedHalf<T>(data: Array<T>): Array<T> {
+  return data.filter( (el, index) => index < data.length / 2);
+}
+
+console.log(getSplitedHalf(
+  [1,2,3,4, 5]
+));
+
+
+getSplitedHalf<string>([
+  'dd',
+  'sd',
+  'dscbhsd'
+]);
