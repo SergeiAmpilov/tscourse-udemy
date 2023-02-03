49ce05ce6898627eaef8b9880bef63242ce11a78
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-03 17:48:59 +0300

    lesson 69 first

---
+++ 069/app.js
@@ -0,0 +1,22 @@
+"use strict";
+/* Задача - реализовать функцию, которая принимает два параметра -
+  объект и строку название ключа.
+  возвращает новую структуру с группировкой по ключу данных из первого аргумента */
+const data = [
+    { group: 1, name: 'Vasia' },
+    { group: 2, name: 'Petia' },
+    { group: 1, name: 'Sveta' },
+];
+function groupByKey(arObj, key) {
+    const res = {};
+    arObj.map((el) => {
+        const locKey = el[key];
+        if (!res[locKey]) {
+            res[locKey] = [];
+        }
+        res[locKey].push(el);
+        return el;
+    });
+    return res;
+}
+console.log(groupByKey(data, 'group'));
---
+++ 069/app.ts
@@ -0,0 +1,45 @@
+/* Задача - реализовать функцию, которая принимает два параметра - 
+  объект и строку название ключа.
+  возвращает новую структуру с группировкой по ключу данных из первого аргумента */
+
+
+interface Data {
+  group: number;
+  name: string;
+}
+
+
+const data: Data[] = [
+  { group: 1, name: 'Vasia' },
+  { group: 2, name: 'Petia' },
+  { group: 1, name: 'Sveta' },
+];
+
+interface IGroup<T> {
+  [key: string]: T[];
+}
+
+
+function groupByKey<T extends Record<string, any>>(arObj: T[], key: keyof T): IGroup<T> {
+
+  const res: {string: T[]} = {
+    string: []
+  };
+  arObj.map( (el) => {
+    
+    const locKey = el[key];
+    if (!res[locKey]) {
+      res[locKey] = [];
+    }
+
+    res[locKey].push(el);
+    
+    return el;
+  })
+
+
+  return res;
+
+}
+
+console.log(groupByKey(data, 'group'));
