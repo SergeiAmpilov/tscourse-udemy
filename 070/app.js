cd2433c475ee1360fde2bd478dc4c8e39ddb7a57
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-03 19:04:07 +0300

    lesson 70

--- 070/app.js
+++ 070/app.js
@@ -1,3 +1,13 @@
 "use strict";
 let strOrNum;
 strOrNum = Math.random() > .5 ? 5 : 'hw';
+let str2OrNumber;
+const user70 = {
+    name: 'Vasilisa'
+};
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "Up";
+    Direction["Down"] = "Down";
+})(Direction || (Direction = {}));
+;
--- 070/app.ts
+++ 070/app.ts
@@ -1,3 +1,19 @@
 let strOrNum: string | number;
 strOrNum = Math.random() > .5 ? 5 : 'hw';
 
+let str2OrNumber: typeof strOrNum;
+
+const user70 = {
+  name: 'Vasilisa'
+};
+
+type keyOfUser = keyof typeof user70;
+
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+};
+
+
+type a70 = keyof typeof Direction;
