4ee175212567b9ba2222a187b8f31a85b5267fb6
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-03 20:00:11 +0300

    one more case for lesson 71

--- 071/app.js
+++ 071/app.js
@@ -5,7 +5,10 @@ const user71 = {
         { name: 'admin' },
         { name: 'editor' },
         { name: 'user' },
-    ]
+    ],
+    permission: {
+        endDate: new Date()
+    }
 };
 const userName = user71['name'];
 const roles71 = ['admin', 'user', 'editor'];
--- 071/app.ts
+++ 071/app.ts
@@ -2,9 +2,14 @@ interface Role71 {
   name: string;
 }
 
+interface Permission {
+  endDate: Date;
+}
+
 interface User71 {
   name: string;
   roles: Role71[];
+  permission: Permission;
 }
 
 const user71: User71 = {
@@ -13,7 +18,10 @@ const user71: User71 = {
     { name: 'admin' },
     { name: 'editor' },
     { name: 'user' },
-  ]
+  ],
+  permission: {
+    endDate: new Date()
+  }
 };
 
 
@@ -22,6 +30,7 @@ type rolesType = User71['roles'];
 
 
 type roleType = User71['roles'][number];
+type DateType = User71['permission']['endDate'];
 
 
 const roles71 = ['admin', 'user', 'editor'] as const;
